98ab43ed51b99d2ddb8e40967f7899dc025b88b0
rith1x <kiruthikkumarmpc24@gmail.com>
2023-12-21 20:00:46 +0530

    21-12-2023

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+// String 
+
+var a = "Lollipop";
+var b = 1989;
+var c = BigInt(b);
+var d = true;
+var e;
+var f = Symbol('Sample Symbol');
+var g = null;
+
+
+console.log(typeof (a));
+console.log(typeof (b));
+console.log(typeof (c));
+console.log(typeof (d));
+console.log(typeof (e));
+console.log(typeof (f));
+console.log(g === null);
+
+
+
+
+
+
+
+
